5cd9d405f2dd4454a0e581eec5656a3552ff9a20
enrico.scelsa <enrico.scelsa@fftech.org>
2021-09-24 14:13:44 +0200

    some reworks on process; some localizations (in the psetup process) are not working at the moment

---
+++ WscPrinter_Setup/ApiModels/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WscPrinter_Setup.ApiModels {
+  public class UserProfile {
+    string SiteName { get; set; }
+    string TheSkin { get; set; }
+    string TheStyle { get; set; }
+    string UserEmail { get; set; }
+    string ReferenceWebsite { get; set; }
+    string ProductsSold { get; set; }
+    string CompanyType { get; set; }
+    string CompanyLogo { get; set; }
+  }
+}
---
+++ WscPrinter_Setup/ApiModels/WebsiteEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WscPrinter_Setup.ApiModels {
+  public class WebsiteEntity {
+    public string Img { get; set;  }     //  = "/img/sampleWebsitesPreview/DynamicSoft-small.png";
+    public string TemplateId { get; set; }     //  = "XXXXYYYYZZZ";
+    public string TemplateName { get; set; }     //  = "Dynamic Institutional";
+    public string TargetLink { get; set; }     //  = "https://www.dynamicsof.it";
+
+  }
+}
