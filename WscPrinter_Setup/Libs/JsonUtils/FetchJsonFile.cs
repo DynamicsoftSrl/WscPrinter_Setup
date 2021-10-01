5fcff5c9d285969e66448152fde936ec0969b836
enrico.scelsa <enrico.scelsa@fftech.org>
2021-10-01 16:42:13 +0200

    server side startup

---
+++ WscPrinter_Setup/Libs/JsonUtils/FetchJsonFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WscPrinter_Setup.Libs.JsonUtils {
+  public static class FetchJsonFile {
+
+    public static T LoadJson<T>(string fileName) {
+      // deserialize JSON directly from a file
+      string jsonString = File.ReadAllText(fileName);
+      T item = JsonSerializer.Deserialize<T>(jsonString);
+
+      return item;
+    }
+  }
+}
+
+
+
+
