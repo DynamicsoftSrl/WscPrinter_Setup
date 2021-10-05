5866f9767de86557fe5e42749913985dcc54173e
enrico.scelsa <enrico.scelsa@fftech.org>
2021-10-05 03:01:25 +0200

    revised file upload; sparse fixes from yesterday; validations;many links/buttons back in anchor format; ravised step7 layout;

--- WscPrinter_Setup/ApiModels/UserProfile.cs
+++ WscPrinter_Setup/ApiModels/UserProfile.cs
@@ -39,21 +39,21 @@ namespace WscPrinter_Setup.ApiModels {
     public string CompanyLogoData { get; set; }
     public string AuthToken { get; set; }
     public UserProfile4Wsc(UserProfile theWalkingObj) {
-      this.ProductsSold = quickArrayToString(theWalkingObj.ProductsSold);
-      this.OtherProductsSold = theWalkingObj.OtherProductsSold;
-      this.ReferenceWebsite = quickArrayToString(theWalkingObj.ReferenceWebsite);
-      this.OtherReferenceWebsite = theWalkingObj.OtherReferenceWebsite;
-      this.CompanyType = quickArrayToString(theWalkingObj.CompanyType);
-      this.OtherCompanyType = theWalkingObj.OtherCompanyType;
-      this.TheStyle = theWalkingObj.TheStyle;
-      this.TheSkin = theWalkingObj.TheSkin;
-      this.SiteName = theWalkingObj.SiteName;
-      this.UserEmail = theWalkingObj.UserEmail;
-      this.UserPhone = theWalkingObj.UserPhone;
-      this.UserDeclaredName = theWalkingObj.UserDeclaredName;
+      this.ProductsSold = quickArrayToString(theWalkingObj.ProductsSold) ?? "";
+      this.OtherProductsSold = theWalkingObj.OtherProductsSold ?? "";
+      this.ReferenceWebsite = quickArrayToString(theWalkingObj.ReferenceWebsite) ?? "";
+      this.OtherReferenceWebsite = theWalkingObj.OtherReferenceWebsite ?? "";
+      this.CompanyType = quickArrayToString(theWalkingObj.CompanyType) ?? "";
+      this.OtherCompanyType = theWalkingObj.OtherCompanyType ?? "";
+      this.TheStyle = theWalkingObj.TheStyle ?? "";
+      this.TheSkin = theWalkingObj.TheSkin ?? "";
+      this.SiteName = theWalkingObj.SiteName ?? "";
+      this.UserEmail = theWalkingObj.UserEmail ?? "";
+      this.UserPhone = theWalkingObj.UserPhone ?? "";
+      this.UserDeclaredName = theWalkingObj.UserDeclaredName ?? "";
 
-      this.CompanyLogo = theWalkingObj.CompanyLogo;
-      this.CompanyLogoData = theWalkingObj.CompanyLogoData;
+      this.CompanyLogo = theWalkingObj.CompanyLogo ?? "";
+      this.CompanyLogoData = theWalkingObj.CompanyLogoData ?? "";
       this.AuthToken = theWalkingObj.AuthToken;
 
     }
