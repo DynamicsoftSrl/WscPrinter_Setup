8b7646a3e81ec589163cd174022bb01490423a46
DynamicsoftSrl <info@dynamicsoft.it>
2021-10-22 14:34:56 +0200

    Completed Template preview. Fixed others steps

--- WscPrinter_Setup/Pages/WscBuilder/TemplatePreview/TemplatePreview.cshtml.cs
+++ WscPrinter_Setup/Pages/WscBuilder/TemplatePreview/TemplatePreview.cshtml.cs
@@ -5,17 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using WscPrinter_Setup.ApiModels;
+using WscPrinter_Setup.Libs.Session;
 
 namespace WscPrinter_Setup.Pages.WscBuilder.TemplatePreview
 {
+    
+
     public class TemplatePreviewModel : PageModel
     {
+        public string CustomerName { get; set; }
         public string LinkUrl { get; set; }
         public void OnGet()
         {
+           
         }
         public void OnPost(string thelink)
         {
+            var theWalkingUser = HttpContext.Session.GetObjectFromJson<UserProfile>("USER_PROFILE", new UserProfile());
+
+            this.CustomerName = theWalkingUser.SiteName;
             this.LinkUrl = thelink;
         }
 
